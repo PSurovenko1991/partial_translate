371d08bd3a68d9f3cdeb1117df5f78e0b96f22bb
pablo <grdg2010@mail.ru>
2017-04-12 14:39:39 +0600

    модуль перевода(transl.py) работает

--- transl.py
+++ transl.py
@@ -1,8 +1,15 @@
-import requests
-url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?'
-key = 'ВАШ API КЛЮЧ'
-text = 'ТЕКСТ ДЛЯ ПЕРЕВОДА'
-lang = 'ru-en'
-r = requests.post(url, data={'key': key, 'text': text, 'lang': lang})
-# Выводим результат
-print(r.text)
+
+def trans(text1):
+    import requests
+    url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?'
+    key = 'trnsl.1.1.20170412T082822Z.106aadd2c087fc91.dfdbc4cde60d6cd54d9cd5b764ab06c27a4fd4bd'
+    text = text1
+    lang = 'ru-en'
+    r = requests.post(url, data={'key': key, 'text': text, 'lang': lang})
+    *_,t,_ = (r.text).split('"')
+    # Выводим результат
+    print(t)
+
+if __name__ == "__main__":
+    x = input("введите текст для перевода(ru-en): ")
+    trans(x)
