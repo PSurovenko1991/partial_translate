946565a818530e2f18b54ce4c2da3549b956ef4f
pablo <grdg2010@mail.ru>
2017-04-22 02:47:52 +0600

    заменены 2 функции, нормализует слова, находит наиболее встречающиеся из слов и их индексы в корпусе

--- transl.py
+++ transl.py
@@ -4,7 +4,7 @@
 def trans(text1):
     import requests
     url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?'
-    key = 'trnsl.1.1.20170412T082822Z.106aadd2c087fc91.dfdbc4cde60d6cd54d9cd5b764ab06c27a4fd4bd'
+    key = ''
     text = text1
     lang = 'ru-en'
     r = requests.post(url, data={'key': key, 'text': text, 'lang': lang})
